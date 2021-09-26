2c4f6dc2fc2938b5f692794742178a6e4432454b
Maksym-Sharafan <magsimsharafa@gmail.com>
2021-09-26 20:24:06 +0300

    fixed listContact

--- model/contacts/listContacts.js
+++ model/contacts/listContacts.js
@@ -1,5 +1,11 @@
-const contacts = require("./contacts.json")
+const fs = require('fs/promises')
+const path = require('path')
 
-const listContacts = async () => contacts
+const contactsPath = path.join(__dirname, 'contacts.json')
+
+const listContacts = async () => {
+    const allContacts = await fs.readFile(contactsPath, 'utf-8')
+    return JSON.parse(allContacts);
+}
 
 module.exports = listContacts
