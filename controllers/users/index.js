d351ef18b5bd67d7d9fd54f90a54c10baf86d796
Maksym-Sharafan <magsimsharafa@gmail.com>
2021-10-27 15:36:07 +0300

    done

---
+++ controllers/users/index.js
@@ -0,0 +1,9 @@
+const updateAvatar = require("./updateAvatar");
+const getCurrentUser = require("./getCurrentUser");
+const updateSubscription = require("./updateSubscription");
+
+module.exports = {
+    updateAvatar,
+    getCurrentUser,
+    updateSubscription
+}
